abf26077cf87e73468a06637ed13091c789709a2
ivanmikh <ivanmikh@protonmail.com>
2019-06-05 13:48:26 +0900

    new stuff for past months:  - disabled bunch of plugins in vim;  - added new shortcuts to vimrc;  - other minor changes.

--- svn/svndiff.sh
+++ svn/svndiff.sh
@@ -10,25 +10,33 @@ else
     echo "No differences found!"
 fi
 
-for file in $(cat $LIST); do
-    echo "Next file is $file"
-    echo "Press: 'Enter' to see diff"
-    echo "       'q'     to exit"
-    echo "       any other input to skip"
-    read input
-
-    if [ "" == "$input" ]; then
-        svn diff $file
-    elif [ "q" == "$input" ]; then
-        break
-    fi
-done
-
 if [ "update" == "$1" ]; then
     for file in $(cat $LIST); do
         svn update $file
     done
+    echo "Press enter"
+    read
 elif [ "commit" == "$1" ]; then
     svn commit --targets $LIST
+    echo "Press enter"
+    read
+elif [ "revert" == "$1" ]; then
+    svn revert --targets $LIST
+    echo "Press enter"
+    read
+else
+    for file in $(cat $LIST); do
+        echo "Next file is $file"
+        echo "Press: 'Enter' to see diff"
+        echo "       'q'     to exit"
+        echo "       any other input to skip"
+        read input
+    
+        if [ "" == "$input" ]; then
+            svn diff $file
+        elif [ "q" == "$input" ]; then
+            break
+        fi
+    done
 fi
 
