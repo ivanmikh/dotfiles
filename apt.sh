6ea84272727313bdd6cf703f337100553540f3ba
Ivan Mikhanchuk <ivanmikh@pm.me>
2025-03-11 16:35:50 -0700

    Misc updates

--- apt.sh
+++ apt.sh
@@ -43,15 +43,13 @@ pck=(
   "git-email" \
   "gparted" \
   "gzip" \
+  "htop" \
   "kernel-wedge" \
   "kexec-tools" \
   "libelf-dev" \
   "libfontconfig1-dev" \
   "libfreetype6-dev" \
   "libncurses-dev" \
-  "libncurses5" \
-  "libncurses5" \
-  "libncurses5-dev" \
   "libncurses5-dev" \
   "libncurses6" \
   "liborc-dev" \
@@ -88,23 +86,20 @@ pck=(
   "trash-cli" \
   "universal-ctags" \
   "unzip" \
-  "vim-gtk" \
+  "vim-gtk3" \
   "yarn" \
   "yasm" \
   "zsh"
 )
 
 py_pck=(
-  "autopep8" \
-  "cppman" \
-  "ipython" \
-  "matplotlib" \
-  "neovim-remote" \
-  "numpy" \
   "pylint" \
-  "pyvisa" \
-  "pyvisa-py" \
-  "scipy"
+  "python3-ipython" \
+  "python3-matplotlib" \
+  "python3-numpy" \
+  "python3-pyvisa" \
+  "python3-pyvisa-py" \
+  "python3-scipy"
 )
 
 dotfiles=$(readlink -f "$(dirname $0)")
@@ -114,58 +109,45 @@ if [ "$dotfiles" == "" ]; then
 fi
 
 ### install software
-sudo apt update
-sudo apt install "${pck[@]}" -y
-pip3 install "${py_pck[@]}"
+#sudo apt update
+#sudo apt install "${pck[@]}" -y
+#sudo apt install "${py_pck[@]}" -y
 
 ### alacritty
 cargo install alacritty
 sudo update-alternatives --install /usr/bin/alacritty alacritty "${HOME}/.cargo/bin/alacritty" 50
 sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/alacritty 50
 
-function backup_and_link() {
-    if [ -f "${2}" ]; then
-        mv "${2}" "${2}".bk
-    fi
-    ln -sf "${1}" "${2}"
-}
-
 ### bash
-backup_and_link "${dotfiles}"/sh/bashrc ~/.bashrc
+cp --backup "${dotfiles}"/sh/bashrc ~/.bashrc
 
 ### zsh
-backup_and_link "${dotfiles}"/sh/zshrc ~/.zshrc
+cp --backup "${dotfiles}"/sh/zshrc ~/.zshrc
 
 ### aliases
-backup_and_link "${dotfiles}"/sh/aliases ~/.aliases
+cp --backup "${dotfiles}"/sh/aliases ~/.aliases
 
 ### default shell
 sudo chsh --shell /bin/zsh ${USER}
 
-### caps to esc
-gsettings set org.gnome.desktop.input-sources xkb-options \[\'caps:escape\'\]
-
 ### ranger
 mkdir -p ~/.config/ranger
-backup_and_link "${dotfiles}"/ranger/commands.py ~/.config/ranger/commands.py
-backup_and_link "${dotfiles}"/ranger/commands_full.py ~/.config/ranger/commands_full.py
-backup_and_link "${dotfiles}"/ranger/rc.conf ~/.config/ranger/rc.conf
-backup_and_link "${dotfiles}"/ranger/rifle.conf ~/.config/ranger/rifle.conf
-backup_and_link "${dotfiles}"/ranger/scope.sh ~/.config/ranger/scope.sh
+cp --backup "${dotfiles}"/ranger/commands.py ~/.config/ranger/commands.py
+cp --backup "${dotfiles}"/ranger/commands_full.py ~/.config/ranger/commands_full.py
+cp --backup "${dotfiles}"/ranger/rc.conf ~/.config/ranger/rc.conf
+cp --backup "${dotfiles}"/ranger/rifle.conf ~/.config/ranger/rifle.conf
+cp --backup "${dotfiles}"/ranger/scope.sh ~/.config/ranger/scope.sh
 
 ### tmux
-backup_and_link "${dotfiles}"/tmux/tmux.conf ~/.tmux.conf
+cp --backup "${dotfiles}"/tmux/tmux.conf ~/.tmux.conf
 
 ### alacritty
 mkdir -p ~/.config/alacritty
-backup_and_link "${dotfiles}"/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml 
-
-sudo wget -O /usr/share/pixmaps/Alacritty.svg https://raw.githubusercontent.com/alacritty/alacritty/master/extra/logo/compat/alacritty-term.png
-sudo wget -O /usr/share/applications/Alacritty.desktop https://raw.githubusercontent.com/alacritty/alacritty/master/extra/linux/Alacritty.desktop
+cp --backup "${dotfiles}"/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml 
 
 ### scripts
-ln -sf "${dotfiles}"/svn/diff_dirs.sh ~/.local/bin/diff_dirs
-ln -sf "${dotfiles}"/svn/svndiff.sh ~/.local/bin/svndiff
+mkdir -p ~/.local/bin
+ln -sf "${dotfiles}"/svn/svndiff.sh ~/.local/bin/svndiff.sh
 
 ### add cargo bin to profile
 if [ "$(grep -e '.cargo/bin' ~/.profile)" == "" ]; then
@@ -174,7 +156,7 @@ cat << EOF >> ~/.profile
 
 # add Cargo bin directory
 if [ -d "\$HOME/.cargo/bin" ] ; then
-PATH="\$HOME/.cargo/bin:\$PATH"
+    PATH="\$HOME/.cargo/bin:\$PATH"
 fi
 EOF
 
